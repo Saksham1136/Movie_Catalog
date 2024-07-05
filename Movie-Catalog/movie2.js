e877047440828a5abdf77783168b8f199056ea55
Saksham Singh <155532123+Saksham1136@users.noreply.github.com>
2024-07-05 15:49:57 +0530

    Add files via upload

---
+++ Movie-Catalog/movie2.js
@@ -0,0 +1,13 @@
+// link the both login and registaration page
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click' ()=> {
+  wrapper.classList.add('active')
+});
+
+loginLinkLink.addEventListener('click' ()=> {
+  wrapper.classList.add('active')
+});
